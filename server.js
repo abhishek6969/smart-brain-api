a4bec6dfb4032cc22b212f1ca2332d01d677cc1e
abhishek6969 <sunkaleabhishek@gmail.com>
2022-05-04 15:48:43 +0530

    changes database

--- server.js
+++ server.js
@@ -21,11 +21,9 @@ app.use(cors());
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
-    port : 5432,
-    user : 'postgres',
-    password : 'abhishek',
-    database : 'smart-brain'
+    connectionString : 'process.env.DATABASE_URL',
+    ssl : true,
+
   }
 });
 
